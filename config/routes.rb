6f8453ebe5623b374c5c35f56c39dc83dcd779d3
me@danielrivers.com <me@danielrivers.com>
2022-01-01 11:59:24 +0000

    Change to routes and added simple forms and haml gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
+# Rails.application.routes.draw do
+#   get 'home/index'
+#   root "static_pages#landing_page"
+#   get "privacy_policy", to: "static_pages#privacy_policy"
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+# end
+
 Rails.application.routes.draw do
-  root "static_pages#landing_page"
-  get "privacy_policy", to: "static_pages#privacy_policy"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root 'home#index'
 end
