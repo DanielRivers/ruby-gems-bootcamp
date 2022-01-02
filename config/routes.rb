3fa710af3694d1e8284f19004bacb2d4152e1481
me@danielrivers.com <me@danielrivers.com>
2022-01-02 16:34:41 +0000

    add courses pages and rich text input for description

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
-# Rails.application.routes.draw do
-#   get 'home/index'
-#   root "static_pages#landing_page"
-#   get "privacy_policy", to: "static_pages#privacy_policy"
-#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
-# end
 
 Rails.application.routes.draw do
+  resources :courses
   get 'home/index'
   root 'home#index'
 end
